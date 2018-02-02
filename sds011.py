e61ef07e93616f6c52c29fcf7803d6bcd34b6630
Ivan Kalchev <ivan.kalchev@icloud.com>
2018-02-02 23:27:31 +0200

    Fixing the totally wrong initial commit.

--- sds011.py
+++ sds011.py
@@ -1,70 +1,168 @@
+"""This module provides an abstraction for the SDS011 air partuclate densiry sensor.
 """
-"""
-import time
-#from sensors.SDS011 import SDS011 as AirSensor
-from pyhap.accessories._sds011 import SDS011 as AirSensor
-from pyhap.accessory import Accessory, Category
-import pyhap.loader as loader
+import struct
+import serial
 
+#TODO: Commands against the sensor should read the reply and return success status.
 
-class SDS011(Accessory):
+class SDS011(object):
+    """Provides method to read from a SDS011 air particlate density sensor
+    using UART.
+    """
 
-    category = Category.SENSOR
+    HEAD = b'\xaa'
+    TAIL = b'\xab'
+    CMD_ID = b'\xb4'
 
-    def __init__(self, serial_port, *args, **kwargs):
-        self.pm25_density = None
-        self.pm10_density = None
-        super(SDS011, self).__init__(*args, **kwargs)
-        self.serial_port = serial_port
-        self.sensor = AirSensor(serial_port)
+    # The sent command is a read or a write
+    READ = b"\x00"
+    WRITE = b"\x01"
 
-    def _set_services(self):
-        super(SDS011, self)._set_services()
-        char_loader = loader.get_char_loader()
+    REPORT_MODE_CMD = b"\x02"
+    ACTIVE = b"\x00"
+    PASSIVE = b"\x01"
 
-        air_quality_pm25 = loader.get_serv_loader().get("AirQualitySensor")
-        pm25_size = char_loader.get("AirParticulateSize")
-        pm25_size.set_value(0, should_notify=False)
-        self.pm25_density = char_loader.get("AirParticulateDensity")
-        pm25_name = char_loader.get("Name")
-        pm25_name.set_value("PM2.5", should_notify=False)
-        air_quality_pm25.get_characteristic("AirQuality")\
-                        .set_value(1, should_notify=False)
-        air_quality_pm25.add_opt_characteristic(pm25_name, pm25_size, self.pm25_density)
+    QUERY_CMD = b"\x04"
 
-        air_quality_pm10 = loader.get_serv_loader().get("AirQualitySensor")
-        pm10_size = char_loader.get("AirParticulateSize")
-        pm10_size.set_value(1, should_notify=False)
-        self.pm10_density = char_loader.get("AirParticulateDensity")
-        pm10_name = char_loader.get("Name")
-        pm10_name.set_value("PM10", should_notify=False)
-        air_quality_pm10.get_characteristic("AirQuality")\
-                        .set_value(1, should_notify=False)
-        air_quality_pm10.add_opt_characteristic(pm10_name, pm10_size, self.pm10_density)
+    # The sleep command ID
+    SLEEP_CMD = b"\x06"
+    # Sleep and work byte
+    SLEEP = b"\x00"
+    WORK = b"\x01"
 
-        self.add_service(air_quality_pm25)
-        self.add_service(air_quality_pm10)
+    # The work period command ID
+    WORK_PERIOD_CMD = b'\x08'
 
-    def __getstate__(self):
-        state = super(SDS011, self).__getstate__()
-        state["sensor"] = None
-        return state
+    def __init__(self, serial_port, baudrate=9600, timeout=2,
+                 use_query_mode=True):
+        """Initialise and open serial port.
+        """
+        self.ser = serial.Serial(port=serial_port,
+                                 baudrate=baudrate,
+                                 timeout=timeout)
+        self.ser.flush()
+        self.set_report_mode(active=not use_query_mode)
 
-    def __setstate__(self, state):
-        self.__dict__.update(state)
-        self.sensor = AirSensor(self.serial_port)
+    def _execute(self, cmd_bytes):
+        """Writes a byte sequence to the serial.
+        """
+        self.ser.write(cmd_bytes)
 
-    def run(self):
-        #TODO: read sleep status and if necessary, wake up and set dummy values
-        # until it spins properly
-        pm25, pm10 = self.sensor.query()
-        self.pm25_density.set_value(pm25)
-        self.pm10_density.set_value(pm10)
-        self.sensor.sleep()
-        while not self.run_sentinel.wait(30 * 60):
-            self.sensor.sleep(sleep=False)
-            time.sleep(15)
-            pm25, pm10 = self.sensor.query()
-            self.pm25_density.set_value(pm25)
-            self.pm10_density.set_value(pm10)
-            self.sensor.sleep()
+    def _get_reply(self):
+        """Read reply from device."""
+        raw = self.ser.read(size=10)
+        data = raw[2:8]
+        if (sum(d for d in data) & 255) != raw[8]:
+            return None  #TODO: also check cmd id
+        return raw
+
+    def cmd_begin(self):
+        """Get command header and command ID bytes.
+        @rtype: list
+        """
+        return self.HEAD + self.CMD_ID
+
+    def set_report_mode(self, read=False, active=False):
+        """Get sleep command. Does not contain checksum and tail.
+        @rtype: list
+        """
+        cmd = self.cmd_begin()
+        cmd += (self.REPORT_MODE_CMD
+                + (self.READ if read else self.WRITE)
+                + (self.ACTIVE if active else self.PASSIVE)
+                + b"\x00" * 10)
+        cmd = self._finish_cmd(cmd)
+        self._execute(cmd)
+        self._get_reply()
+
+    def query(self):
+        """Query the device and read the data.
+
+        @return: Air particulate density in micrograms per cubic meter.
+        @rtype: tuple(float, float) -> (PM2.5, PM10)
+        """
+        cmd = self.cmd_begin()
+        cmd += (self.QUERY_CMD
+                + b"\x00" * 12)
+        cmd = self._finish_cmd(cmd)
+        self._execute(cmd)
+
+        raw = self._get_reply()
+        if raw is None:
+            return None  #TODO:
+        data = struct.unpack('<HH', raw[2:6])
+        pm25 = data[0] / 10.0
+        pm10 = data[1] / 10.0
+        return (pm25, pm10)
+
+    def sleep(self, read=False, sleep=True):
+        """Sleep/Wake up the sensor.
+
+        @param sleep: Whether the device should sleep or work.
+        @type sleep: bool
+        """
+        cmd = self.cmd_begin()
+        cmd += (self.SLEEP_CMD
+                + (self.READ if read else self.WRITE)
+                + (self.SLEEP if sleep else self.WORK)
+                + b"\x00" * 10)
+        cmd = self._finish_cmd(cmd)
+        self._execute(cmd)
+        self._get_reply()
+
+    def set_work_period(self, read=False, work_time=0):
+        """Get work period command. Does not contain checksum and tail.
+        @rtype: list
+        """
+        assert work_time >= 0 and work_time <= 30
+        cmd = self.cmd_begin()
+        cmd += (self.WORK_PERIOD_CMD
+                + (self.READ if read else self.WRITE)
+                + bytes([work_time])
+                + b"\x00" * 10)
+        cmd = self._finish_cmd(cmd)
+        self._execute(cmd)
+        self._get_reply()
+
+    def _finish_cmd(self, cmd, id1=b"\xff", id2=b"\xff"):
+        """Add device ID, checksum and tail bytes.
+        @rtype: list
+        """
+        cmd += id1 + id2
+        checksum = sum(d for d in cmd[2:]) % 256
+        cmd += bytes([checksum]) + self.TAIL
+        return cmd
+
+    def _process_frame(self, data):
+        """Process a SDS011 data frame.
+
+        Byte positions:
+            0 - Header
+            1 - Command No.
+            2,3 - PM2.5 low/high byte
+            4,5 - PM10 low/high
+            6,7 - ID bytes
+            8 - Checksum - sum of bytes 2-7
+            9 - Tail
+        """
+        raw = struct.unpack('<HHxxBBB', data[2:])
+        checksum = sum(v for v in data[2:8]) % 256
+        if checksum != data[8]:
+            return None
+        pm25 = raw[0] / 10.0
+        pm10 = raw[1] / 10.0
+        return (pm25, pm10)
+
+    def read(self):
+        """Read sensor data.
+
+        @return: PM2.5 and PM10 concetration in micrograms per cude meter.
+        @rtype: tuple(float, float) - first is PM2.5.
+        """
+        byte = 0
+        while byte != self.HEAD:
+            byte = self.ser.read(size=1)
+            d = self.ser.read(size=10)
+            if d[0:1] == b"\xc0":
+                data = self._process_frame(byte + d)
+                return data
